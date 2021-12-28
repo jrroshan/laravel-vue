d344730725169c8f37ca04f1a4c8e158086d3b88
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-12-28 13:45:02 +0545

    added get delete and upload

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,12 +10,39 @@ class AdminController extends Controller
     public function addTag(Request $request)
     {
         // Validate
-        return $create = Tag::create([
+        $this->validate($request ,[
+            'tagName' =>'required'
+        ]);
+        return Tag::create([
             'tagName' => $request->tagName
+        ]);
+    }
+
+    public function editTag(Request $request){
+        // Validate
+        $this->validate($request ,[
+            'tagName' =>'required',
+            'id'=>'required',
+        ]);
+        return Tag::where('id',$request->id)->update([
+            'tagName' =>$request->tagName,
         ]);
     }
     
     public function getTag(){
         return Tag::orderBy('id','desc')->get();
+    }
+
+    public function deleteTag(Request $request){
+        $this->validate($request,[
+            'id' => 'required',
+        ]);
+        return Tag::where('id',$request->id)->delete();
+    }
+
+    public function upload(Request $request){
+        $picName = time().'.'.$request->file->extension();
+        $request->file->move(public_path('uploads'),$picName);
+        return $picName;
     }
 }
